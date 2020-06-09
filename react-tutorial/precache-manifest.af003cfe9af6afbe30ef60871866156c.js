ec552b6bcaf436a890afc2e510b2ae64395ef240
Subin Abid <subinabid@gmail.com>
2020-06-09 14:21:59 +0530

    Revert "smm"
    This reverts commit e73c8850a6cc4655b2f6740fb585b00dc99d8b49.

---
+++ react-tutorial/precache-manifest.af003cfe9af6afbe30ef60871866156c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d1ed49db77f134bd17789770c29563",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2bcaf86b1bde1464c4e3",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "6c3ec90a605c1be697ae",
+    "url": "/react-tutorial/static/js/2.22339641.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/react-tutorial/static/js/2.22339641.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2bcaf86b1bde1464c4e3",
+    "url": "/react-tutorial/static/js/main.b1e1960f.chunk.js"
+  },
+  {
+    "revision": "d08d96db01afc9216673",
+    "url": "/react-tutorial/static/js/runtime-main.1c060b1b.js"
+  }
+]);
